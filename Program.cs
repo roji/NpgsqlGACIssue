26ff0ea27d4dd0e7f65593af861d2143351bdee6
Shay Rojansky <roji@roji.org>
2020-03-01 10:58:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Npgsql;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var conn = new NpgsqlConnection("Host=localhost;Database=test;Username=test;Password=test"))
+            {
+                conn.Open();
+            }
+        }
+    }
+}
